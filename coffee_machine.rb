829bade25d27869b538d84dd0ad6ec182596e015
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-12-06 15:36:20 +0100

    second iteration TDD

--- coffee_machine.rb
+++ coffee_machine.rb
@@ -1,14 +1,21 @@
 class CoffeeMachine
-  def initialize(beverage, sugar, stick)
+  def initialize(beverage, sugar, stick, price)
     @beverage = beverage
     @sugar = sugar
     @stick = stick
+    @price = price
   end
 
   def coffee_machine
-    return 'T:1:0' if @beverage == 'tea'
-    return 'H::' if @beverage == 'chocolate'
-    return 'C:2:0' if @beverage == 'coffee'
+    return 'T:1:0' if @beverage == 'tea' && @price == 0.4
+    return "Sorry: #{(0.4 - @price).round(1)}€ is missing" if @beverage == 'tea' && @price < 0.4
+
+    return 'H::' if @beverage == 'chocolate' && @price == 0.5
+    return "Sorry: #{(0.5 - @price).round(1)}€ is missing" if @beverage == 'chocolate' && @price < 0.5
+
+    return 'C:2:0' if @beverage == 'coffee'  && @price == 0.6
+    return "Sorry: #{(0.6 - @price).round(1)}€ is missing" if @beverage == 'coffee' && @price < 0.6
+
     return 'M:message-content'
   end
 end
--- spec/coffee_machine_spec.rb
+++ spec/coffee_machine_spec.rb
@@ -3,33 +3,48 @@ require_relative '../coffee_machine'
 describe CoffeeMachine do
 
   describe "#coffee_machine" do
-
-    it "should return 'T:1:0' when customer orders one tea with one sugar and a stick'" do
-      order = CoffeeMachine.new('tea', 'sugar', 'stick')
-      actual = order.coffee_machine
+    it "should return 'T:1:0' when customer orders one tea with one sugar and a stick" do
+      order = CoffeeMachine.new('tea', 1, 1, 0.4).coffee_machine
       expected = 'T:1:0'
-      expect(actual).to eq(expected)
+      expect(order).to eq(expected)
     end
 
     it "should return 'H::' when customer orders one chocolate with no sugar and therefore no stick" do
-      order = CoffeeMachine.new('chocolate', 'sugar', 'stick')
-      actual = order.coffee_machine
+      order = CoffeeMachine.new('chocolate', 0, 0, 0.5).coffee_machine
       expected = 'H::'
-      expect(actual).to eq(expected)
+      expect(order).to eq(expected)
     end
 
     it "should return 'C:2:0' when customer orders one coffee with two sugars and a stick" do
-      order = CoffeeMachine.new('coffee', 'sugar', 'stick')
-      actual = order.coffee_machine
+      order = CoffeeMachine.new('coffee', 2, 1, 0.6).coffee_machine
       expected = 'C:2:0'
-      expect(actual).to eq(expected)
+      expect(order).to eq(expected)
+    end
+
+    it "should return 'Sorry: 0.2€ is missing' is missing when customer a tea" do
+      order = CoffeeMachine.new('tea', 1, 1, 0.2).coffee_machine
+      expected = "Sorry: #{0.4 - 0.2}€ is missing"
+      expect(order).to eq(expected)
+    end
+
+    it "should return 'Sorry: 0.3€ is missing' when customer orders a chocolate" do
+      order = CoffeeMachine.new('chocolate', 1 , 1, 0.2).coffee_machine
+      expected = "Sorry: #{(0.5 - 0.2).round(1)}€ is missing"
+      expect(order).to eq(expected)
+    end
+
+
+    it "should return 'Sorry: 0.4€ is missing' when customer orders a coffe" do
+       order = CoffeeMachine.new('coffee', 1, 1, 0.2).coffee_machine
+      expected = "Sorry: #{(0.6 - 0.2).round(1)}€ is missing"
+      expect(order).to eq(expected)
     end
 
     it "should return 'M:message-content' when there is no message" do
-      order = CoffeeMachine.new('', '', '')
-      actual = order.coffee_machine
+      order = CoffeeMachine.new('', '', '', '').coffee_machine
       expected = 'M:message-content'
-      expect(actual).to eq(expected)
+      expect(order).to eq(expected)
     end
   end
 end
+
